95261cdb3fa745224bbeabd579972db2df060158
rlaxoghk7 <jjun0660@gmail.com>
2025-07-30 09:26:04 +0900

    0730수정

--- js/05_inquiry_contact_answer.js
+++ js/05_inquiry_contact_answer.js
@@ -14,21 +14,15 @@ id: "1234",
   },
   {
     question: "맞춤형(커스터마이징) 제작이 가능한가요? 가능하다면 어떤 정보를 제공해야 하나요?",
-    questionLink: "../05_inquiry_contact-3.html",   
-    answer: "답변이 등록되었습니다.",
-    answerLink: "../05_inquiry_contact_answer.html" 
+    answer: "답변 대기중 입니다.",
   },
   {
     question: "모터 설치 및 유지보수는 직접 해야 하나요, 아니면 기술 지원이 제공되나요?",
-    questionLink: "../05_inquiry_contact-3.html",   
-    answer: "답변이 등록되었습니다.",
-    answerLink: "../05_inquiry_contact_answer.html" 
+    answer: "답변 대기중 입니다.",
   },
   {
-    question: "에너지 효율 등급이나 절전형 제품이 따로 있나요?",
-    questionLink: "../05_inquiry_contact-3.html",   
-    answer: "답변이 등록되었습니다.",
-    answerLink: "../05_inquiry_contact_answer.html" 
+    question: "에너지 효율 등급이나 절전형 제품이 따로 있나요?",  
+    answer: "답변 대기중 입니다.",
   }
 ];
 
@@ -49,19 +43,23 @@ function renderFAQ(data) {
     faqItem.classList.add("faq-item");
 
     faqItem.innerHTML = `
-      <div class="faq-box">
-        <div class="faq-label q">Q</div>
-        <div class="faq-content">
-          <a href="${item.questionLink}" >${item.question}</a>
-        </div>
-      </div>
-      <div class="faq-box answer">
-        <div class="faq-label a">A</div>        
-        <div class="faq-content">
-          <a href="${item.answerLink}" >${item.answer}</a>
-        </div>
-      </div>
-    `;
+  <div class="faq-box">
+    <div class="faq-label q">Q</div>
+    <div class="faq-content">
+      <a href="${item.questionLink}">${item.question}</a>
+    </div>
+  </div>
+  <div class="faq-box answer">
+    <div class="faq-label a ${item.answer === "답변 대기중 입니다." ? "gray-label" : ""}">A</div>        
+    <div class="faq-content">
+      ${
+        item.answer === "답변 대기중 입니다."
+          ? `<p class="no-link">${item.answer}</p>`
+          : `<a href="${item.answerLink}">${item.answer}</a>`
+      }
+    </div>
+  </div>
+`;
 
     faqListElement.appendChild(faqItem);
   });
